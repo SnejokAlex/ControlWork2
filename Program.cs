339ce119bcf50d017f074c082074237e7b4d722a
SnejokAlex <sanya4266405@yandex.ru>
2023-03-13 23:13:12 +0400

    Enter code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int MaxArrayLength = 3;
+Console.WriteLine("Enter your text");
+string array1 = Console.ReadLine();
+Console.WriteLine("Enter your 2 text");
+string array2 = Console.ReadLine();
+Console.WriteLine("Enter your 3 text");
+string array3 = Console.ReadLine();
+Console.WriteLine("Enter your 4 text");
+string array4 = Console.ReadLine();
+if (MaxArrayLength>=array1.Length)
+{
+    Console.WriteLine(array1);
+}
+if (MaxArrayLength>=array2.Length)
+{
+    Console.WriteLine(array2);
+}
+if (MaxArrayLength>=array3.Length)
+{
+    Console.WriteLine(array3);
+}
+if (MaxArrayLength>=array4.Length)
+{
+    Console.WriteLine(array4);
+}
+
